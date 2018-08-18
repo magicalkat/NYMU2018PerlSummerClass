573ff35bc490edfe51e94656eabd2e46a48229a4
蘇子翔 <jimmy850210@gmail.com>
2018-08-19 03:30:11 +0800

    bug fixed

--- 99.pl
+++ 99.pl
@@ -1,7 +1,7 @@
 #!/user/bin/env perl
 
-for($i=1;$i<1000;$i++){
-    for($j=1;$j<1000;$j++){
+for($i=1;$i<10;$i++){
+    for($j=1;$j<10;$j++){
         printf "%3d X %3d = %6d",$i,$j,$i*$j;
         print "\t";
     }
